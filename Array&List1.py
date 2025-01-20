28968eec3a64a2b9edb457532f2643639ebfa7b2
Shreyash Lodhi <shreyashlodhi2305@gmail.com>
2025-01-20 21:20:29 +0530

    Array & List

---
+++ Array&List1.py
@@ -0,0 +1,29 @@
+from array import *
+
+a1 = array("i", [23, 56, 11])
+
+# print(type(a1))
+# print(a1)
+
+# for x in a1:
+#     print(x)
+
+# for i in range(3):
+#     print(a1[i], end=" ")
+
+# i = 0
+# while i < len(a1):
+#     print(a1[i])
+#     i += 1
+
+# Array methods
+a1.append(20)
+# print(a1)
+# print(a1.count(11))
+# print(a1.index(11))
+# a1.pop()
+# print(a1)
+# a1.pop(1)
+# print(a1)
+# a1.remove(56)
+# print(a1)
